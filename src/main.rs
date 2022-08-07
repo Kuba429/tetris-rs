dbc89b78d3c14ff41615e9470aefa61e33b87522
Kuba429 <jakubzawadzki429@gmail.com>
2022-08-07 12:53:37 +0200

    get random shape

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod tile;
 
 use grid::*;
 use macroquad::prelude::*;
-use piece::get_shape;
+use piece::{get_random_shape_template_getter, get_shape};
 use tile::Tile;
 
 pub const BASE: u16 = 250;
@@ -27,7 +27,7 @@ async fn main() {
     let (x, y): (u8, u8) = (4, 5);
     let mut shape_template: Vec<u8> = vec![0, 0, 0, 0, 1, 0, 1, 1, 1];
     let mut shape: Vec<Option<Tile>> = get_shape(x, y, shape_template);
-
+    let get_random_shape = get_random_shape_template_getter();
     shape.iter().for_each(|tile| {
         if let Some(t) = tile {
             grid[t.x as usize][t.y as usize] = t.val;
@@ -37,6 +37,8 @@ async fn main() {
     loop {
         clear_background(WHITE);
         draw_grid(&grid);
+        shape_template = get_random_shape();
+        println!("{}", shape_template[0]);
         // current_piece.move_to(BOTTOM);
         next_frame().await;
     }
--- src/piece.rs
+++ src/piece.rs
@@ -1,5 +1,25 @@
-use crate::tile::Tile;
+use std::sync::{Arc, Mutex};
 
+use crate::tile::Tile;
+use rand::prelude::*;
+// #[rustfmt::skip] // easier to see the shapes
+pub fn get_random_shape_template_getter() -> impl Fn() -> Vec<u8> {
+    let shape_templates: Arc<Mutex<[Vec<u8>]>> = Arc::new(Mutex::new([
+        vec![0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        vec![0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0],
+        vec![0, 3, 3, 3, 3, 0, 0, 0, 0],
+        vec![4, 4, 0, 0, 4, 4, 0, 0, 0],
+        vec![5, 0, 0, 5, 5, 5, 0, 0, 0],
+        vec![0, 0, 6, 6, 6, 6, 0, 0, 0],
+        vec![0, 7, 0, 7, 7, 7, 0, 0, 0],
+    ]));
+    let rng = Arc::new(Mutex::new(rand::thread_rng()));
+    return move || -> Vec<u8> {
+        let shapes = shape_templates.lock().unwrap();
+        let random_idx = rng.lock().unwrap().gen_range(0..shapes.len());
+        return (*shapes[random_idx]).try_into().unwrap();
+    };
+}
 pub fn get_shape(x: u8, y: u8, template: Vec<u8>) -> Vec<Option<Tile>> {
     return match template.len() {
         9 => template
